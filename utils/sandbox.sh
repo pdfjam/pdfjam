88619d29f29e7b2c4fa050ae5b2663e8e4501ed4
Markus Kurtz <m@mgkurtz.de>
2025-01-30 01:19:23 +0100

    Add tests for images and file names

--- utils/sandbox.sh
+++ utils/sandbox.sh
@@ -3,9 +3,14 @@ cd "$(dirname "$0")/.."
 
 rm -rf build/bin
 mkdir -p build/bin
-for i in awk basename cat cp cut dirname extractbb file grep iconv kpsewhich ln lualatex mkdir mv od paperconf pdfinfo pdflatex realpath rm sed tee tr tty uname xelatex; do
-	ln -s "$(command -v $i)" build/bin/$i || { echo "No $i command found. Something is seriously amiss."; exit 1; }
+for i in awk basename cat cp cut dirname extractbb file grep gs iconv kpsewhich ln lualatex mkdir mv od paperconf pdfinfo pdflatex realpath repstopdf rungs rm sed tee texlua tr tty uname xelatex; do
+	ln -s "$(command -v "$i")" "build/bin/$i" || { echo "No $i command found. Something is seriously amiss."; exit 1; }
 done
 
 mkdir build/bin/libpaper
 mv build/bin/paperconf build/bin/libpaper/
+
+## For clueless people: do a "binary search" for several needles here and watch until all works out
+# bin="$(pwd)/build/bin"
+# cd /usr/bin
+# for i in [a-z]*; do ln -sr "$i" "$bin/" done
