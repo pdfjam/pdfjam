96e16d4ad5a0679298f9d0dee9b80b25454e41f9
Markus Kurtz <m@mgkurtz.de>
2025-01-30 01:28:25 +0100

    Fixup workflow

--- utils/sandbox.sh
+++ utils/sandbox.sh
@@ -4,8 +4,9 @@ cd "$(dirname "$0")/.."
 rm -rf build/bin
 mkdir -p build/bin
 for i in awk basename cat cp cut dirname extractbb file grep gs iconv kpsewhich ln lualatex mkdir mv od paperconf pdfinfo pdflatex realpath repstopdf rungs rm sed tee texlua tr tty uname xelatex; do
-	ln -s "$(command -v "$i")" "build/bin/$i" || { echo "No $i command found. Something is seriously amiss."; exit 1; }
+	ln -s "$(command -v "$i")" "build/bin/$i" || { echo "No $i command found. Something is seriously amiss."; error=1; }
 done
+[ -n "$error" ] && exit 1
 
 mkdir build/bin/libpaper
 mv build/bin/paperconf build/bin/libpaper/
