1f4fc75be4b9c8d774d33ff71bb4bfc3437c7831
Markus Kurtz <m@mgkurtz.de>
2025-02-04 15:33:12 +0100

    Add util to update man page date

---
+++ utils/man-date.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd "$(dirname -- "$0")"/..
+man=doc/pdfjam.1
+if [ -n "$(git status --porcelain "$man")" ]; then
+	if [ -n "$(git diff --ignore-matching-lines='^\.TH PDFJAM' HEAD "$man")" ]; then
+		date=$(date +%F)
+	else
+		date=$(git log --format=%as "$man"|sort|tail -n1)
+	fi
+fi
+sed -Ei "1s/\"[0-9]{4}-[0-9]{2}-[0-9]{2}\"/\"$date\"/" "$man"
