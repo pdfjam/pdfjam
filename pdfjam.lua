dd3536efde91376c56df0ae66562275eaffaef0c
Markus Kurtz <m@mgkurtz.de>
2025-02-11 04:02:38 +0100

    working prototype

--- pdfjam.lua
+++ pdfjam.lua
@@ -1,7 +1,10 @@
 #!/usr/bin/env texlua
+kpse.set_program_name("pdfjam")
 version="N.NN"
 
 -- utility
+
+require "lualibs"
 
 function coalesce(...) -- return first non-nil
 	for i = 1, select("#", ...) do
@@ -9,24 +12,26 @@ function coalesce(...) -- return first non-nil
 	end
 end
 
-function append(t, a) t[#t+1] = a end
-
-function split(s, d, plain)
-	local r = {}
-	local a, b = 0, string.find(s, d, 1, plain)
-	while b do
-		append(r, string.sub(s, a+1, b-1))
-		a, b = b, string.find(s, d, b+1, plain)
-	end
-	append(r, string.sub(s, a+1))
-	return r
+--function rmdir(t) os.spawn({"rm", "-rf", t[1]}) end
+function rmdir(t) end
+function tmpdir()
+	local x = os.tmpname()
+	os.remove(x)
+	if not lfs.mkdir(x) then error("Could not create tmpdir") end
+	local t = {x}
+	setmetatable(t, {__gc = rmdir})
+	return t
 end
 
 -- option parser
+
 Parser = {}
 
 function Parser:new(obj)
 	local obj = obj or {}
+	obj.options = obj.options or {}
+	obj.short_options = obj.short_options or {}
+	obj.no_options = obj.no_options or ""
 	self.__index = self
 	setmetatable(obj, self)
 	return obj
@@ -41,18 +46,17 @@ function Parser:parse(args)
 		self:parse_word(args[self.i])
 		self.i = self.i + 1
 	end
-	return self.t, self.as
+	return self.as, self.t
 end
 
 function Parser:parse_word(word)
-	if dash_seen or string.sub(word, 1, 1) ~= "-" then
-		self:parse_positional_argument(word)
-	elseif string.sub(word, 1, 2) ~= "--" then
-		self:parse_short_options(word)
-	elseif word ~= "--" then
-		self:parse_long_option(string.sub(word, 3))
-	else
+	if not self.dash_seen and word == "--" then
 		self.dash_seen = true
+	elseif not self.dash_seen and string.sub(word, 1, 2) == "--" then
+		self:parse_long_option(string.sub(word, 3))
+	elseif self.dash_seen or string.sub(word, 1, 1) ~= "-"
+		or not self:parse_short_options(word) then
+		self:parse_positional_argument(word)
 	end
 end
 
@@ -60,17 +64,19 @@ function Parser:parse_long_option(a)
 	local v
 	local p = string.find(a, "=", 2, true)
 	if p then v = string.sub(a, p+1) a = string.sub(a, 1, p-1) end
-	self:set_option(self.options[a], a, v)
+	self:set_option(a, v)
 end
 
 function Parser:parse_short_options(word)
-	assert(#word >= 2)
 	self.j = 2
 	local s = string.sub(word, 2, 2)
+	if string.find(self.no_options, s, 1, true) then return false end
 	while s ~= "" do
 		a = self.short_options[s]
 		if a then
-			self:set_option(self.options[a], a)
+			self:set_option(a)
+		elseif string.find(self.no_options, s, 1, true) then
+			self:error("invalid short option -" .. s)
 		else
 			self:warn("unknown option -" .. s)
 		end
@@ -78,18 +84,20 @@ function Parser:parse_short_options(word)
 		s = string.sub(word, self.j, self.j)
 	end
 	self.j = nil
+	return true
 end
 
-function Parser:set_option(o, a, v)
+function Parser:set_option(a, v)
 	self.positional_argument_read = nil
-	if type(o) == "string" then -- alias
-		return self:set_option(self.options[o], o, v)
-	elseif string.sub(a, 1, 3) == "no-" then
+	if string.sub(a, 1, 3) == "no-" then
 		self:unset(string.sub(a, 4))
 		if v then self:error("unsetting expects no value") end
 		return
 	end
-	o = o or normalopt
+	o = self.options[a] or normalopt
+	if type(o) == "string" then -- alias
+		return self:set_option(o, v)
+	end
 	assert(type(o) == "table")
 	if o.argument == "required" then
 		v = v or self:argument()
@@ -106,30 +114,25 @@ function Parser:set_option(o, a, v)
 end
 
 function Parser:parse_positional_argument(word)
-	if self.positional_argument_read then
-		append(self.as[#self.as], word)
-	else
-		self.positional_argument_read = true
-		append(self.as, {word})
-	end
+	table.insert(self.as, word)
 end
 
 function Parser:set(a, value) if value ~= nil then self.t[a] = value end end
 function Parser:unset(a) self.t[a] = nil end
-function Parser:word(j) return self._args[self.i + (j or 0)] end
+function Parser:word(n) return self._args[self.i + (n or 0)] end
 
 function Parser:argument()
 	local w
 	if self.j then
-		w = string.sub(self:word(), j+1)
-		if w then return w end
+		w = string.sub(self:word(), self.j+1)
+		if w ~= "" then self.j = #self:word() return w end
 	end
 	w = self:word(1)
 	if not w then
 		self:error("argument required, but end of command line reached")
 	elseif string.sub(w, 1, 1) == "-" then
-		self:error("argument required, but the next word starts with '-'. \z
-			Use --option=value syntax for values starting with '-'")
+		self:error("argument required, but the following word '" .. w ..
+			"' starts with '-'. Use --option=value syntax for values starting with '-'")
 	end
 	self.i = self.i+1
 	return w
@@ -137,6 +140,7 @@ end
 
 function Parser:error(s, ...)
 	if not s then return end
+	collectgarbage()
 	error(string.format("Error while parsing %s: " .. s .. ".", self:word(), ...))
 end
 
@@ -144,8 +148,6 @@ function Parser:warn(s, ...)
 	if not s then return end
 	print(string.format("Warning while parsing %s: " .. s .. ".", self:word(), ...))
 end
-
--- pdfjam specific
 
 -- pdfjam options
 
@@ -226,7 +228,7 @@ function define_options()
 	end}
 	options.preamble = {argument = "required", func = function(v, _, t)
 		t.preamble = t.preamble or {}
-		append(t.preamble, v)
+		table.insert(t.preamble, v)
 	end}
 	options.longedge = "otheredge"
 	options.shortedge = "no-otheredge"
@@ -234,18 +236,29 @@ function define_options()
 end
 
 short_options = {h="help", V="version", q="quiet", o="outfile"}
+no_options = ",-0123456789l" -- explicitely no short options
+-- all other chars may be used in future pdfjam versions
 
-PdfjamParser = Parser:new()
+function exit(code) collectgarbage() os.exit(code, true) end
+function show_help() print("This is how to use it.") exit() end
+function show_version() print("pdfjam version "..version) exit() end
+function show_configpath() print("configpath is ...") exit() end
 
-function PdfjamParser:parse_short_options(word)
-	if self:is_valid_pagespec(word) then return end
-	return Parser.parse_short_options(self, word)
+function getopt()
+	parser = Parser:new({
+		options = define_options(),
+		short_options = short_options,
+		no_options = no_options
+	})
+	return parser:parse(arg)
 end
 
-function PdfjamParser:is_valid_pagespec(word)
-	for _, r in ipairs(split(word, ",", true)) do
+-- pdfjam
+
+function is_valid_pagespec(word)
+	for _, r in ipairs(string.split(word, ",", true)) do
 		if r ~= "{}" then
-			local pages = split(r, "-", true)
+			local pages = string.split(r, "-", true)
 			if #pages > 2 then return false end
 			for _, p in ipairs(pages) do
 				if not (tonumber(p) or p == "" or p == "last") then
@@ -257,36 +270,65 @@ function PdfjamParser:is_valid_pagespec(word)
 	return true
 end
 
-function exit(code) os.exit(code, true) end
-function show_help() print("This is how to use it.") exit() end
-function show_version() print("pdfjam version "..version) exit() end
-function show_configpath() print("configpath is ...") exit() end
-function quiet() end
-
-function getopt()
-	parser = PdfjamParser:new({options=define_options(), short_options=short_options})
-	return parser:parse(arg)
-end
-
-function dump(t, indent)
-	if type(t) == "table" and (not indent or #indent < 4) then
-		if indent then print("...") else indent = "" end
-		for k, v in pairs(t) do
-			io.write(indent .. k .. " = ")
-			dump(v == t and "(self)" or v, indent.."\t")
-		end
-		m = getmetatable(t)
-		if m then
-			io.write(indent .. "metatable -> ")
-			dump(m, indent.."\t")
-		end
-	else
-		print(t)
+function with_pagespec(l, ps)
+	local r = {}
+	for _, a in ipairs(l) do
+		table.insert(r, a)
+		table.insert(r, ps)
 	end
+	return r
 end
 
-t1, t2 = getopt()
-print("options")
-dump(t1)
-print("arguments")
-dump(t2)
+template = [[
+\documentclass[%s]{article}
+\usepackage[%s]{geometry}
+\usepackage[utf8]{inputenc}
+\usepackage{pdfpages}
+\begin{document}
+\includepdfmerge[%s]{%s}
+\end{document}]]
+
+-- main
+
+function main()
+	local as, opts = getopt()
+
+	local dd = tmpdir()
+	local d = dd[1]
+
+	local list = {}
+	local l = {}
+	for i, x in ipairs(as) do
+		if l and is_valid_pagespec(x) then
+			table.append(list, with_pagespec(l, x))
+			l = {}
+		else
+			if not io.exists(x) then error("File '"..x.."' does not exist.") end
+			local y = file.join(d, "source-"..i..".pdf")
+			file.copy(x, y)
+			table.insert(l, y)
+		end
+	end
+	table.append(list, with_pagespec(l, "-"))
+
+	local pwd = lfs.currentdir()
+	local outfile = file.join(pwd, "out.pdf")
+	lfs.chdir(d)
+
+	local content = string.format(template, "", "", "", table.concat(list, ","))
+
+	local f = io.open("a.tex", "w")
+	f:write(content)
+	f:close()
+	os.spawn({"pdflatex", "a.tex"})
+	file.copy("a.pdf", outfile)
+end
+
+--success, msg = pcall(main)
+success, msg = pcall(main)
+collectgarbage()
+if not success then
+	print("pdfjam run unsuccessfull.")
+	print(msg)
+	exit(1)
+end
