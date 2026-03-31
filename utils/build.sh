f516d08f842a0c3e51a945cb6b155981eb6cb31e
Markus Kurtz <m@mgkurtz.de>
2026-03-31 20:24:12 +0200

    ci: install latexmk and ghostscript

--- utils/build.sh
+++ utils/build.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")/.."
 target=build/pdfjam
 version="$1"
 
-if command -v latexmk &>/dev/null && command -v ghostscript &>/dev/null; then
+if command -v latexmk >/dev/null && command -v ghostscript >/dev/null; then
 	(cd testfiles/support/src && latexmk && pdfjam --a4paper beamer.pdf; ghostscript -o a4+square.pdf -sDEVICE=pdfwrite -sPageList=1 a4.pdf square.pdf)
 else
 	echo "Warning: LatexMk and Ghostscript needed to create example files"
