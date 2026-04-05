d5a91df856393549519e0a6cd728514a46a36e3d
Markus Kurtz <m@mgkurtz.de>
2026-04-05 17:25:22 +0200

    ✅(completion) group completions

--- testfiles/completion/support/zsh.sh
+++ testfiles/completion/support/zsh.sh
@@ -1,47 +1,64 @@
-#!/bin/zsh -fi
+#!/bin/zsh
 
 # https://unix.stackexchange.com/questions/668618/how-to-write-automated-tests-for-zsh-completion
 
-# BUG: In an _alternative, this script only catches the completion of the last alternative.
+scriptname="$0"
+usage() { echo "Usage: $scriptname [ -p prefix] [-s file] [-d debuglog] (--|input) [input ...]"; exit 1; }
 
-usage() { echo "Usage: $0 [ -p prefix] [-s file] [-d debuglog] [--] input [input ...]"; exit 1; }
-
-prefix=
-source=
-debug=cat
+local source=
+local prefix=
+local debug=cat
+local -a ignore
+local accept_no_files=
 while :; do
 	case "$1" in
 		-s) source="$2" && shift 2 || usage ;;
 		-p) prefix="$2" && shift 2 || usage ;;
 		-d) debug=(tee -a "$2") && shift 2 || usage ;;
-		--) shift ;&
+		-i) ignore+="$2" && shift 2 || usage ;;
+		--) accept_no_files=1; shift ;&
 		*) break ;;
 	esac
 done
 
+[[ $# = 0 ]] && [[ -z "$accept_no_files" ]] && usage
+
+# Simulate completion by invoking the zle (zsh command line editor) in a zpty (pseudo terminal)
+#
+# After running this function in a zpty, writing "${text}\v" into that zpty
+# will perform completion as if ${text} were written at command position in a command line.
 compfake () {
+	# 0. Assumption: This is called inside a pseudo terminal (i.e. zpty)
+	# 0a) Set the size of this pty to gigantic
+	stty rows 100000 cols 2000
+	# 1. Initialize the completion system
 	autoload -Uz compinit
 	compinit
+	# 2. Setup the completion system
+	# 2a) Source completion scripts into the system
 	[ -n "$source" ] && source "$source"
-	zstyle ':completion:*' list-prompt '<irrelevant>'
-	# matches print as "Xmatch^D", where
+	# 2b) Setup completion styles
+	zstyle ':completion:*:descriptions' format '<HEADER>%d</HEADER>'
+	zstyle ':completion:*' list-separator '<DESCRIPTION>'
+	zstyle ':completion:*' list-prompt '<YOUR-SCREEN-IS-TO-SMALL>' # If you ever see this, just increase the terminal’s size.
+	# completion matches print as "Xmatch^D", where
 	#   X=^A for normal text, X=^F for all kinds of files and directories
-	#   alignment spaces start with ^S, a directory has a trailing ^T/^D
+	#   alignment spaces start with ^S, some files/directories end in ^D^Tx^D with x indicating file type
 	zstyle ':completion:*' list-colors $'no=\CA' lc= rc= tc=$'\CT' $'ec=\CD' $'sp=\CS' \
 		$'fi=\CF' $'di=\CF' $'ln=\CF' $'pi=\CF' $'so=\CF' $'bd=\CF' $'cd=\CF' $'or=\CF' $'mi=\CF' \
 		$'su=\CF' $'su=\CF' $'sg=\CF' $'tw=\CF' $'ow=\CF' $'sa=\CF' $'st=\CF' $'ex=\CF'
-	zstyle ':completion:*' list-separator '<DESCRIPTION>'
-	zstyle ':completion:*:descriptions' format '<HEADER>%d</HEADER>'
-	zstyle ':completion:*' force-list always
-	# Bind a custom widget to \v.
-	bindkey '\v' complete-word
-	zle -C {,,}complete-word
-	complete-word () {
-		unset 'compstate[vared]'  # Ignore that we are in a vared
+	zstyle ':completion:*' group-name '' # Give each tag its own group of completions, possibly naming the same entry twice
+	zstyle ':completion:*' force-list always # Show completions even if only one suggestion exists
+	zstyle ':completion:*' ignored-patterns "$ignore[@]"
+	# 2c) Define and bind a widget to fire the _main_completion function as if in command position and not in a vared.
+	bindkey '\v' fake-complete
+	zle -C fake-complete complete-word breakout-of-vared-completer
+	breakout-of-vared-completer() {
+		unset 'compstate[vared]'
 		_main_complete "$@"
 	}
-	stty rows 100000 cols 2000
-	vared -c tmp # invoke line editor
+	# 3. Profit: Invoke the line editor, waiting for input
+	vared -c tmp
 }
 
 comptest() {
